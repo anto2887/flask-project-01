1f2d239ed32cc0890c5a1d71a2af6b1c7835c50e
uktony <uktony12@gmail.com>
2024-05-31 00:59:48 -0500

    corrected ecs.tf

--- flaskr/ecs.tf
+++ flaskr/ecs.tf
@@ -1,3 +1,25 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "flaskr_ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "flaskr_ecs_cluster" {
   name = "flaskr-ecs-cluster"
 }
@@ -8,6 +30,7 @@ resource "aws_ecs_task_definition" "flaskr_app_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   
   container_definitions = jsonencode([
     {
