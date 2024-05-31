665073b5fe4e16e14b0f8a1a75d3b9c3fcb2b204
uktony <uktony12@gmail.com>
2024-05-31 10:03:26 -0500

    added vpc dns support

--- flaskr/vpc.tf
+++ flaskr/vpc.tf
@@ -2,6 +2,8 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "flaskr_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "flaskr_igw" {
