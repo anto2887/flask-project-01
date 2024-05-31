159d6c0d8cdadaa2a49640267e6b733b59ab4c27
uktony <uktony12@gmail.com>
2024-05-31 10:24:45 -0500

    adding in logging for ecs

--- flaskr/ecs.tf
+++ flaskr/ecs.tf
@@ -20,8 +20,21 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_cluster" "flaskr_ecs_cluster" {
-  name = "flaskr-ecs-cluster"
+resource "aws_iam_role" "ecs_task_role" {
+  name = "flaskr_ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_ecs_task_definition" "flaskr_app_task" {
@@ -31,7 +44,8 @@ resource "aws_ecs_task_definition" "flaskr_app_task" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
   container_definitions = jsonencode([
     {
       name      = "flaskr-app"
@@ -43,6 +57,14 @@ resource "aws_ecs_task_definition" "flaskr_app_task" {
           hostPort      = 80
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/flaskr-app"
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
       environment = [
         {
           name  = "DB_HOST"
