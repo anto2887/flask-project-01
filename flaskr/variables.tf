e02aae808d035e9b23cc0583645fb16dcba55320
uktony <uktony12@gmail.com>
2024-05-31 01:46:26 -0500

    added vpc ednpoints and variables.tf

---
+++ flaskr/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"  # You can set your default region here
+}
--- flaskr/vpc.tf
+++ flaskr/vpc.tf
@@ -1,9 +1,8 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "flaskr_vpc" {
   cidr_block = "10.0.0.0/16"
 }
-
-# Data source to fetch available availability zones
-data "aws_availability_zones" "available" {}
 
 resource "aws_internet_gateway" "flaskr_igw" {
   vpc_id = aws_vpc.flaskr_vpc.id
@@ -24,6 +23,16 @@ resource "aws_subnet" "flaskr_public_subnet" {
   cidr_block              = cidrsubnet(aws_vpc.flaskr_vpc.cidr_block, 8, count.index)
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "flaskr-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "flaskr_public_rt_assoc" {
+  count          = 2
+  subnet_id      = element(aws_subnet.flaskr_public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.flaskr_public_rt.id
 }
 
 resource "aws_subnet" "flaskr_private_subnet" {
@@ -31,6 +40,31 @@ resource "aws_subnet" "flaskr_private_subnet" {
   vpc_id            = aws_vpc.flaskr_vpc.id
   cidr_block        = cidrsubnet(aws_vpc.flaskr_vpc.cidr_block, 8, count.index + 2)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "flaskr-private-subnet-${count.index}"
+  }
+}
+
+# VPC Endpoints for ECR and other services
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.flaskr_vpc.id
+  service_name      = "com.amazonaws.${var.region}.ecr.dkr"
+  subnet_ids        = aws_subnet.flaskr_private_subnet[*].id
+  security_group_ids = [aws_security_group.flaskr_vpc_sg.id]
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.flaskr_vpc.id
+  service_name      = "com.amazonaws.${var.region}.ecr.api"
+  subnet_ids        = aws_subnet.flaskr_private_subnet[*].id
+  security_group_ids = [aws_security_group.flaskr_vpc_sg.id]
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.flaskr_vpc.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  route_table_ids   = [aws_route_table.flaskr_private_rt.id]
 }
 
 resource "aws_lb" "flaskr_app_alb" {
@@ -42,11 +76,11 @@ resource "aws_lb" "flaskr_app_alb" {
 }
 
 resource "aws_lb_target_group" "flaskr_app_tg" {
-  name     = "flaskr-app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.flaskr_vpc.id
-  target_type = "ip"
+  name         = "flaskr-app-tg"
+  port         = 80
+  protocol     = "HTTP"
+  vpc_id       = aws_vpc.flaskr_vpc.id
+  target_type  = "ip"
 
   health_check {
     path                = "/"
