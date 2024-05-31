2f2a5ad0fe8512f359ee7414b471a7edb4574002
uktony <uktony12@gmail.com>
2024-05-31 13:17:10 -0500

    more config tweaks

--- flaskr/s3.tf
+++ flaskr/s3.tf
@@ -32,37 +32,3 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
     ]
   })
 }
-resource "aws_s3_bucket" "alb_logs" {
-  bucket = "flaskr-app-alb-logs"
-
-  tags = {
-    Name        = "flaskr-app-alb-logs"
-    Environment = "production"
-  }
-}
-
-resource "aws_s3_bucket_policy" "alb_logs_policy" {
-  bucket = aws_s3_bucket.alb_logs.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "logging.elb.amazonaws.com"
-        },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.alb_logs.arn}/*"
-      },
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "logging.elb.amazonaws.com"
-        },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.alb_logs.arn}"
-      }
-    ]
-  })
-}
