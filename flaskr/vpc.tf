3ee03194ca8741db5af802c5e2689a31a92c50be
uktony <uktony12@gmail.com>
2024-05-31 09:28:20 -0500

    added vpc sg

--- flaskr/vpc.tf
+++ flaskr/vpc.tf
@@ -60,20 +60,25 @@ resource "aws_route_table_association" "flaskr_private_rt_assoc" {
 resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id            = aws_vpc.flaskr_vpc.id
   service_name      = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
   subnet_ids        = aws_subnet.flaskr_private_subnet[*].id
   security_group_ids = [aws_security_group.flaskr_vpc_sg.id]
+  private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id            = aws_vpc.flaskr_vpc.id
   service_name      = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
   subnet_ids        = aws_subnet.flaskr_private_subnet[*].id
   security_group_ids = [aws_security_group.flaskr_vpc_sg.id]
+  private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.flaskr_vpc.id
   service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.flaskr_private_rt.id]
 }
 
