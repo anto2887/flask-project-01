db7e6849091a4ec421cfb07a78a7637b8bc74549
uktony <uktony12@gmail.com>
2024-06-15 18:33:51 -0500

    added ecs service

--- flaskr/ecs.tf
+++ flaskr/ecs.tf
@@ -91,3 +91,24 @@ resource "aws_ecs_task_definition" "flaskr_app_task" {
     }
   ])
 }
+
+
+resource "aws_ecs_service" "flaskr_ecs_service" {
+  name            = "flaskr-ecs-service"
+  cluster         = aws_ecs_cluster.flaskr_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.flaskr_app_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.flaskr_public_subnet[*].id
+    security_groups  = [aws_security_group.flaskr_ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.flaskr_app_tg.arn
+    container_name   = "flaskr-app"
+    container_port   = 80
+  }
+}
