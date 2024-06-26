b132c7a250221a25579cd6230584cb174f54ef33
uktony <uktony12@gmail.com>
2024-06-25 20:34:26 -0500

    adding dns name

--- flaskr/variables.tf
+++ flaskr/variables.tf
@@ -3,3 +3,8 @@ variable "region" {
   type        = string
   default     = "us-east-1"  # You can set your default region here
 }
+
+variable "acm_certificate_arn" {
+  description = "arn:aws:acm:us-east-1:193482034911:certificate/11a0ef6b-e195-4f02-989b-6d4de71feacc"
+  type        = string
+}
--- flaskr/vpc.tf
+++ flaskr/vpc.tf
@@ -83,10 +83,26 @@ resource "aws_lb_target_group" "flaskr_app_tg" {
   }
 }
 
-resource "aws_lb_listener" "flaskr_app_listener" {
+resource "aws_lb_listener" "flaskr_app_http_listener" {
   load_balancer_arn = aws_lb.flaskr_app_alb.arn
   port              = "80"
   protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "flaskr_app_https_listener" {
+  load_balancer_arn = aws_lb.flaskr_app_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.acm_certificate_arn 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.flaskr_app_tg.arn
@@ -94,8 +110,8 @@ resource "aws_lb_listener" "flaskr_app_listener" {
 }
 
 resource "aws_route53_record" "flaskr_alb" {
-  zone_id = "Z1013243V3H94OXCQ2KV"  # Replace with your Route 53 hosted zone ID
-  name    = "ops76.co"              # Replace with your domain name
+  zone_id = "Z1013243V3H94OXCQ2KV"  
+  name    = "ops76.co"              
   type    = "A"
 
   alias {
