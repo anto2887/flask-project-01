edb4ab48653c90c8a70d5971fbc723d8e6528e93
uktony <uktony12@gmail.com>
2024-07-22 18:27:03 -0500

    updating the frontend top-section card

--- flaskr/app/blog.py
+++ flaskr/app/blog.py
@@ -1,25 +1,81 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from werkzeug.exceptions import abort
+from sqlalchemy import func
 
 from app.auth import login_required
-from app.models import Post, Users, UserResults, db  # Updated import statement
+from app.models import Post, Users, UserResults, UserPredictions, Group, UserGroup, db
+from app.season import get_current_season
 
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 @login_required
 def index():
-    # Get user_requests sorted by points in descending order.
-    user_requests = UserResults.query.order_by(UserResults.points.desc()).all()
+    # Get user's groups
+    user_groups = Group.query.join(UserGroup).filter(UserGroup.user_id == g.user.id).all()
 
-    # Get posts authored by the logged-in user.
-    posts = Post.query.join(Users).filter(  # Updated Users
-        Post.author_id == g.user.id
-    ).order_by(Post.created.desc()).all()
+    # Get available seasons
+    seasons = db.session.query(UserPredictions.season.distinct()).all()
+    seasons = [season[0] for season in seasons]
 
-    return render_template('blog/index.html', user_requests=user_requests, posts=posts)
+    # Get the current group (you might want to add logic to determine the current group)
+    current_group = user_groups[0] if user_groups else None
+
+    # Get all users in the current group
+    if current_group:
+        users = Users.query.join(UserGroup).filter(UserGroup.group_id == current_group.id).all()
+    else:
+        users = [g.user]  # If no group, just show the current user
+
+    # Get user points
+    user_points = db.session.query(
+        UserResults.author_id,
+        func.sum(UserResults.points).label('total_points')
+    ).group_by(UserResults.author_id).all()
+    
+    user_points = {up.author_id: up.total_points for up in user_points}
+
+    # Get the most recent prediction
+    recent_prediction = Post.query.filter_by(author_id=g.user.id).order_by(Post.created.desc()).first()
+
+    return render_template('blog/index.html', 
+                           users=users,
+                           user_points=user_points,
+                           recent_prediction=recent_prediction,
+                           user_groups=user_groups,
+                           seasons=seasons)
+
+@bp.route('/get_filtered_results')
+@login_required
+def get_filtered_results():
+    group_id = request.args.get('group_id')
+    season = request.args.get('season')
+
+    # Query to get filtered results
+    query = db.session.query(
+        Users.id,
+        Users.username,
+        func.coalesce(func.sum(UserResults.points), 0).label('total_points')
+    ).join(UserGroup, Users.id == UserGroup.user_id)
+    
+    if group_id:
+        query = query.filter(UserGroup.group_id == group_id)
+    
+    if season:
+        query = query.filter(UserResults.season == season)
+    
+    query = query.outerjoin(UserResults, Users.id == UserResults.author_id)
+    query = query.group_by(Users.id, Users.username)
+    query = query.order_by(func.sum(UserResults.points).desc())
+
+    results = query.all()
+
+    return jsonify([
+        {'username': result.username, 'points': int(result.total_points)}
+        for result in results
+    ])
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -43,7 +99,7 @@ def create():
     return render_template('blog/create.html')
 
 def get_post(id, check_author=True):
-    post = Post.query.join(Users).filter(Post.id == id).first()  # Updated Users
+    post = Post.query.join(Users).filter(Post.id == id).first()
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
