80f5396040d7ed525bf8cd2c67c4115df8555f53
uktony <uktony12@gmail.com>
2024-07-24 18:52:14 -0500

    updating manage_group page and css

--- flaskr/app/views.py
+++ flaskr/app/views.py
@@ -1,9 +1,36 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
-from app.models import Users, Group, UserGroup, db
+from app.models import Users, Group, db
 from app.forms import CreateGroupForm
 
 group_bp = Blueprint('group', __name__, url_prefix='/group')
+
+@group_bp.route('/manage', methods=['GET', 'POST'])
+@login_required
+def manage_groups():
+    created_groups = Group.query.filter_by(creator_id=current_user.id).all()
+    
+    if request.method == 'POST':
+        group_id = request.form.get('group_id')
+        group = Group.query.get_or_404(group_id)
+        
+        if group.creator_id != current_user.id:
+            flash('You are not authorized to manage this group.', 'danger')
+            return redirect(url_for('group.manage_groups'))
+        
+        form = CreateGroupForm(obj=group)
+        if form.validate():
+            group.name = form.name.data
+            group.league = form.league.data
+            selected_users = Users.query.filter(Users.id.in_(request.form.getlist('selected_users'))).all()
+            group.users = selected_users
+            if current_user not in group.users:
+                group.users.append(current_user)
+            db.session.commit()
+            flash('Group updated successfully!', 'success')
+            return redirect(url_for('group.manage_groups'))
+    
+    return render_template('manage_groups.html', groups=created_groups)
 
 @group_bp.route('/create', methods=['GET', 'POST'])
 @login_required
@@ -14,33 +41,10 @@ def create_group():
     if form.validate_on_submit():
         new_group = Group(name=form.name.data, league=form.league.data, creator_id=current_user.id)
         selected_users = Users.query.filter(Users.id.in_(request.form.getlist('selected_users'))).all()
-        new_group.users = selected_users
+        new_group.users = selected_users + [current_user]
         db.session.add(new_group)
         db.session.commit()
         flash('Group created successfully!', 'success')
-        return redirect(url_for('blog.index'))
+        return redirect(url_for('group.manage_groups'))
 
     return render_template('create_group.html', form=form, users=users)
-
-@group_bp.route('/<int:group_id>/manage', methods=['GET', 'POST'])
-@login_required
-def manage_group(group_id):
-    group = Group.query.get_or_404(group_id)
-
-    if group.creator_id != current_user.id:
-        flash('You are not authorized to manage this group.', 'danger')
-        return redirect(url_for('blog.index'))
-
-    form = CreateGroupForm(obj=group)
-    all_users = Users.query.all()
-
-    if form.validate_on_submit():
-        group.name = form.name.data
-        group.league = form.league.data
-        selected_users = Users.query.filter(Users.id.in_(request.form.getlist('selected_users'))).all()
-        group.users = selected_users
-        db.session.commit()
-        flash('Group updated successfully!', 'success')
-        return redirect(url_for('blog.index'))
-
-    return render_template('manage_group.html', form=form, group=group, users=all_users)
