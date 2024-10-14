a5e079e3b2b4fff79b6a0446293225d5f1cc509e
uktony <uktony12@gmail.com>
2024-10-13 23:41:25 -0500

    more changes

--- flaskr/db.tf
+++ flaskr/db.tf
@@ -8,17 +8,22 @@ resource "aws_db_subnet_group" "flaskr_subnet_group" {
 }
 
 resource "aws_db_instance" "flaskr_db" {
+  identifier           = "flaskrdb"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "13.13" # Updated version
+  engine_version       = "13.13"
   instance_class       = "db.t3.micro"
-  identifier                 = "flaskrdb"
+  name              = "flaskrdb"
   username             = "myuser"
   password             = "mypassword"
   db_subnet_group_name = aws_db_subnet_group.flaskr_subnet_group.name
   vpc_security_group_ids = [aws_security_group.flaskr_rds_sg.id]
   skip_final_snapshot  = true
+  
+  # Add these lines to handle the backup configuration
+  backup_retention_period = 0
+  backup_window           = "03:00-04:00"
 
   tags = {
     Name = "flaskr-db"
