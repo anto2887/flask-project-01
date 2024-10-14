fbda519360c851cb5608b5da18845a95108f9135
uktony <uktony12@gmail.com>
2024-10-13 23:34:17 -0500

    more changes

--- flaskr/provider.tf
+++ flaskr/provider.tf
@@ -1,3 +1,25 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # This will use the latest 4.x version
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.1"
+    }
+  }
+  required_version = ">= 0.14"
 }
+
+provider "aws" {
+  region = var.region
+}
+
+provider "random" {}
+
+provider "null" {}
