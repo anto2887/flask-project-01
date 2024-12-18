bc779fd8cff631150ddcc8ba1cd957ec036a80c5
uktony <uktony12@gmail.com>
2024-12-17 19:59:42 -0600

    adding in bastion host for db testing

---
+++ flaskr/bastion.tf
@@ -0,0 +1,28 @@
+# Bastion host EC2 instance
+resource "aws_instance" "bastion" {
+  ami           = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 22.04 LTS in us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.flaskr_public_subnet[0].id  # Place in first public subnet
+
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name              = "nginxubuntu"
+
+  tags = {
+    Name = "flaskr-bastion"
+  }
+
+  # Install PostgreSQL client on startup
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y postgresql-client
+              EOF
+
+  user_data_replace_on_change = true
+}
+
+# Output the bastion's public IP for easy access
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+  description = "Public IP address of the bastion host"
+}
--- flaskr/sg.tf
+++ flaskr/sg.tf
@@ -65,3 +65,38 @@ resource "aws_security_group" "flaskr_rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Security group for bastion host
+resource "aws_security_group" "bastion_sg" {
+  name        = "flaskr-bastion-sg"
+  description = "Security group for bastion host"
+  vpc_id      = aws_vpc.flaskr_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["64.44.87.157/32"]  # Your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "flaskr-bastion-sg"
+  }
+}
+
+# Add rule to RDS security group to allow access from bastion
+resource "aws_security_group_rule" "rds_from_bastion" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion_sg.id
+  security_group_id        = aws_security_group.flaskr_rds_sg.id
+}
