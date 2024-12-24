e8c1bbd5acde3554bcc1d2571b15176d2f2ec971
uktony <uktony12@gmail.com>
2024-12-23 23:11:37 -0600

    more fixes

--- flaskr/sg.tf
+++ flaskr/sg.tf
@@ -1,5 +1,5 @@
 # Security group for Application Load Balancer
-resource "aws_security_group" "alb_sg" {
+resource "aws_security_group" "flaskr_alb_sg" {
   name        = "flaskr-alb-sg"
   description = "Security group for Application Load Balancer"
   vpc_id      = aws_vpc.flaskr_vpc.id
@@ -27,24 +27,23 @@ resource "aws_security_group" "alb_sg" {
 }
 
 # Security group for ECS instances
-resource "aws_security_group" "ecs_sg" {
+resource "aws_security_group" "flaskr_ecs_sg" {
   name        = "flaskr-ecs-sg"
   description = "Security group for ECS instances"
   vpc_id      = aws_vpc.flaskr_vpc.id
 
-  # Allow traffic from ALB
   ingress {
     from_port       = 5000
     to_port         = 5000
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.flaskr_alb_sg.id]
   }
 
   ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.flaskr_alb_sg.id]
   }
 
   egress {
@@ -56,25 +55,23 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 # Security group for RDS
-resource "aws_security_group" "rds_sg" {
+resource "aws_security_group" "flaskr_rds_sg" {
   name        = "flaskr-rds-sg"
   description = "Security group for RDS database"
   vpc_id      = aws_vpc.flaskr_vpc.id
 
-  # Allow traffic from ECS instances
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
+    security_groups = [aws_security_group.flaskr_ecs_sg.id]
   }
 
-  # Allow traffic from Bastion Host
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id]
+    security_groups = [aws_security_group.flaskr_bastion_sg.id]
   }
 
   egress {
@@ -86,12 +83,11 @@ resource "aws_security_group" "rds_sg" {
 }
 
 # Security group for Bastion Host
-resource "aws_security_group" "bastion_sg" {
+resource "aws_security_group" "flaskr_bastion_sg" {
   name        = "flaskr-bastion-sg"
   description = "Security group for bastion host"
   vpc_id      = aws_vpc.flaskr_vpc.id
 
-  # Allow SSH access from a specific IP (replace with your own IP)
   ingress {
     from_port   = 22
     to_port     = 22
@@ -105,24 +101,4 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-# Add rule to ECS security group to allow traffic from ALB
-resource "aws_security_group_rule" "ecs_from_alb" {
-  type                     = "ingress"
-  from_port                = 5000
-  to_port                  = 5000
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.alb_sg.id
-  security_group_id        = aws_security_group.ecs_sg.id
-}
-
-# Add rule to RDS security group to allow access from Bastion Host
-resource "aws_security_group_rule" "rds_from_bastion" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.bastion_sg.id
-  security_group_id        = aws_security_group.rds_sg.id
 }
