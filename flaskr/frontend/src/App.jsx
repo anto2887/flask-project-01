934ee1e834e00a705fbb72dbf59e5e24239bc8c4
uktony <uktony12@gmail.com>
2025-01-24 16:05:57 -0600

    fixing auth0 references

--- flaskr/frontend/src/App.jsx
+++ flaskr/frontend/src/App.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Auth0ProviderWithConfig } from './auth/auth0-provider-with-config';
-import { PrivateRoute } from './components/common/PrivateRoute';
-import { useAuth } from './hooks/useAuth';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+import { GroupForm } from './components/groups/GroupForm';
 
-// Import your existing components here
-// import { GroupForm } from './components/groups/GroupForm';
-// import { TeamSelector } from './components/groups/TeamSelector';
-
-const App = () => {
-  return (
-    <Auth0ProviderWithConfig>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <DashboardPage />
-              </PrivateRoute>
-            }
-          />
-          {/* Add more routes as needed */}
-        </Routes>
-      </Router>
-    </Auth0ProviderWithConfig>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/group/create" element={<GroupForm />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
