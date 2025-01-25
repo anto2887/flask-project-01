12611d8cd28013e3b6c13b7757d0efe4794027e7
uktony <uktony12@gmail.com>
2025-01-25 13:32:33 -0600

    more frontend changes, adding in a webpack plugin

--- flaskr/frontend/webpack.config.js
+++ flaskr/frontend/webpack.config.js
@@ -1,5 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,25 +13,19 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['babel-loader']
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
   resolve: {
     extensions: ['.js', '.jsx']
   },
   devServer: {
-    historyApiFallback: true,
-    hot: true,
-    port: 3000
+    historyApiFallback: true
   }
 };
